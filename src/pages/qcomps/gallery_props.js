6d60b6512dcab55f98eea8e79378814275e5089f
Owen <seltzer.o@northeastern.edu>
2024-02-09 16:58:09 -0500

    Class activities for 020924

--- src/pages/qcomps/gallery_props.js
+++ src/pages/qcomps/gallery_props.js
@@ -2,54 +2,48 @@ export default function Gallery() {
   return (
     <div>
       <h1>Notable Scientists</h1>
-      <section className="profile">
-        <h2>Maria Skłodowska-Curie</h2>
-        <img
-          className="avatar"
-          src='https://i.imgur.com/szV5sdGs.jpg'
-          alt="Maria Skłodowska-Curie"
-          width={70}
-          height={70}
-        />
-        <ul>
-          <li>
-            <b>Profession: </b>
-            physicist and chemist
-          </li>
-          <li>
-            <b>Awards: 4 </b>
-            (Nobel Prize in Physics, Nobel Prize in Chemistry, Davy Medal, Matteucci Medal)
-          </li>
-          <li>
-            <b>Discovered: </b>
-            polonium (element)
-          </li>
-        </ul>
-      </section>
-      <section className="profile">
-        <h2>Katsuko Saruhashi</h2>
-        <img
-          className="avatar"
-          src='https://i.imgur.com/YfeOqp2s.jpg'
-          alt="Katsuko Saruhashi"
-          width={70}
-          height={70}
-        />
-        <ul>
-          <li>
-            <b>Profession: </b>
-            geochemist
-          </li>
-          <li>
-            <b>Awards: 2 </b>
-            (Miyake Prize for geochemistry, Tanaka Prize)
-          </li>
-          <li>
-            <b>Discovered: </b>
-            a method for measuring carbon dioxide in seawater
-          </li>
-        </ul>
-      </section>
+      <Profile
+        name="Maria Skłodowska-Curie"
+        image="https://i.imgur.com/szV5sdGs.jpg"
+        profession="physicist and chemist"
+        awards={[
+          "Nobel Prize in Physics",
+          "Nobel Prize in Chemistry",
+          "Davy Medal",
+          "Matteucci Medal"
+        ]}
+        discovery="polonium (element)"
+      />
+      <Profile
+        name="Katsuko Saruhashi"
+        image="https://i.imgur.com/YfeOqp2s.jpg"
+        profession="geochemist"
+        awards={["Miyake Prize for geochemistry", "Tanaka Prize"]}
+        discovery="a method for measuring carbon dioxide in seawater"
+      />
     </div>
+  );
+}
+
+function Profile({ name, image, profession, awards, discovery }) {
+  return (
+    <section className="profile">
+      <h2>{name}</h2>
+      <img className="avatar" src={image} alt={name} width={70} height={70} />
+      <ul>
+        <li>
+          <b>Profession: </b>
+          {profession}
+        </li>
+        <li>
+          <b>Awards: {awards.length} </b>
+          ({awards.join(", ")})
+        </li>
+        <li>
+          <b>Discovered: </b>
+          {discovery}
+        </li>
+      </ul>
+    </section>
   );
 }
