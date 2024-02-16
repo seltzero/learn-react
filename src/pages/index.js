32a5a21661ebcb3e45b5fed5bd329b6aec2fec58
Owen <seltzer.o@northeastern.edu>
2024-02-16 16:59:55 -0500

    Add new components and update existing components

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,11 +3,17 @@
 // import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 import Profile from './components/profile'
+//import List from './qcomps/recipes'
+import RecipeList from './qcomps/recipes'
+import Gallery from './qcomps/state'
+import Form from './qcomps/stuckForm'
+import FeedbackForm from './qcomps/thankYouCrash'
+import Counter from './components/snapshot2'
 
 export default function Home() {
   return (
     <div className={styles.main}>
-        <Profile />
+        <Counter />
     </div>
   )
 }
